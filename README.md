cccb566d4bee8826a537559b829c422e048b8319
Puguh Wijayanto <psw@metalgenix.com>
2016-09-04 02:54:34 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# summernote-ext-codewrapper
-Code Wrapper for Summernote Editor
+# Summernote Codewrapper Extension
+Code Wrapper for Summernote Editor, this will wrap your code into one part. Not like the default code styler by summernote.
+
+## WHY ?
+I made this plugin because the default Summernote had is not wrap the code into one part when using summernote's code styler. It will break into paragraph. And when the code is long, we had to edit it manually. 
+
+When copying and paste the code, summernote can wrap html code but not the others. 
+
+see videos for the demo
+https://www.youtube.com/watch?v=TTUgPvp6DQs
+
+## Installation
+call your plugin script after summernote 
+<script src="http://path.to/your/summernote-ext-codewrapper.min.js"></script>
+
+load the summernote 
+```js
+$(document).ready(function() {
+     $('.editor').summernote({
+         height: 300,
+         toolbar: [
+                 ['insert', ['gxcode']]
+             ]
+     });
+ }
+```
+
+## License
+**MIT License**
---
+++ src/summernote-ext-codewrapper.js
@@ -0,0 +1,67 @@
+(function (factory) {
+    /* global define */
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(window.jQuery);
+    }
+}(function ($) {
+    
+    // Extends plugins for adding readmore.
+    //  - plugin is external module for customizing.
+    $.extend($.summernote.plugins, {
+        /**
+         * @param {Object} context - context object has status of editor.
+         */
+        'gxcode': function (context) {
+            var self = this;
+            
+            // ui has renders to build ui elements.
+            //  - you can create a button with `ui.button`
+            var ui = $.summernote.ui;
+            
+            // add elfinder button
+            context.memo('button.gxcode', function () {
+                // create button
+                var button = ui.button({
+                    contents: '<i class="fa fa-code"/> Code',
+                    tooltip: 'Code Wrapper',
+                    click: function (event) {
+
+                        var highlight = window.getSelection(),  
+                            spn = document.createElement('pre'),
+                            range = highlight.getRangeAt(0)
+                            highlight = nl2br(highlight, true);
+                        spn.innerHTML = highlight;
+
+                        range.deleteContents();
+                        range.insertNode(spn);
+                    }
+
+                });
+                function nl2br (str, is_xhtml) {   
+                  var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';    
+                  return (str + '').replace(/([^>\r\n\r\n]+?)(\r\n\r\n|\n\r\n\r|\r\r|\n\n)/g, '$1'+ breakTag +'$2');
+                }
+                // create jQuery object from button instance.
+                var $elfinder = button.render();
+                return $elfinder;
+            });
+            
+            
+            
+            // This methods will be called when editor is destroyed by $('..').summernote('destroy');
+            // You should remove elements on `initialize`.
+            this.destroy = function () {
+                this.$panel.remove();
+                this.$panel = null;
+            };
+        },
+        
+    });
+}));
